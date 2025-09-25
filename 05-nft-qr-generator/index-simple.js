6cc51fb2bcd172ff1e5942be293e1f885ed2d0da
AlexDesk <alexdesk@example.com>
2025-09-24 23:22:18 -0400

    Fix rate limit error by adding trustProxy option to rate limiter

--- 05-nft-qr-generator/index-simple.js
+++ 05-nft-qr-generator/index-simple.js
@@ -31,7 +31,8 @@ app.use(express.static('admin-panel'));
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
+  max: 100, // limit each IP to 100 requests per windowMs
+  trustProxy: true // Trust proxy for Vercel
 });
 app.use(limiter);
 
